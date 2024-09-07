67b24596e2e0bb0f43af486f61da4d8c50272d77
alberrtval <alberrto.ramos@gmail.com>
2024-09-06 18:58:32 -0600

    changes vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginRewriteAll()],
+  build: {
+    outDir: "dist", // Asegúrate de que Vite coloque los archivos construidos en `dist`
+  },
 });
