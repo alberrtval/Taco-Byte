331accac4e928086fa8169d3de549eb19a57c392
alberrtval <alberrto.ramos@gmail.com>
2024-09-06 19:51:33 -0600

    Vercel deploy

--- node_modules/@fortawesome/free-solid-svg-icons/faCopy.js
+++ node_modules/@fortawesome/free-solid-svg-icons/faCopy.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 var prefix = 'fas';
 var iconName = 'copy';
-var width = 512;
+var width = 448;
 var height = 512;
 var aliases = [];
 var unicode = 'f0c5';
-var svgPathData = 'M224 0c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224zM64 160c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64H64V224h64V160H64z';
+var svgPathData = 'M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z';
 
 exports.definition = {
   prefix: prefix,
