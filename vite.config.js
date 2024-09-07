c57c0d5ec3ff4b1f961116af1a6665d8c8692081
alberrtval <alberrto.ramos@gmail.com>
2024-09-06 19:22:09 -0600

    please work vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginRewriteAll()],
-  build: {
-    outDir: "dist", // Asegúrate de que Vite coloque los archivos construidos en `dist`
-  },
 });
